027771aa9a31f2f1e7c4b897f3472e421441aeca
thibaut <t.barroyer@me.com>
2017-10-24 18:14:12 +0200

    fix compilation

--- cpu/src/model.cc
+++ cpu/src/model.cc
@@ -4,8 +4,9 @@
 # include <stdexcept>
 
 void Model::init_W(size_t m, size_t n) {
-  arma::mat M(m, n, fill::randu);
-  float r = 4.0 * sqrt(6.0 / (m + n))
+  arma::Mat<float> M;
+  M.randu(m, n);
+  float r = 4.0 * sqrt(6.0 / (m + n));
   M *= r;
   this->W.emplace_back(M);
 }
