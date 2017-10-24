4a92c132afdd03003e9123b22241704ae56751d7
thibaut <t.barroyer@me.com>
2017-10-24 16:59:08 +0200

    moving to amadillo

--- cpu/src/model.hh
+++ cpu/src/model.hh
@@ -1,7 +1,5 @@
 # pragma once
 
-# include "type_m.hh"
-
 # include <vector>
 
 class Model {
@@ -9,8 +7,8 @@ class Model {
     Model();
     void add(size_t output_units);
     void add(size_t output_units, size_t input_units);
-    double* predict(Matrix& X);
+    double* predict(double* X);
     void train(size_t nb_epoch=10);
   private:
-    std::vector<Matrix> W;
+    std::vector<double*> W;
 };
