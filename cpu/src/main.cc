891fc296f88955b55e6794fda43833c00e20863d
Ilan Dubois <ilan.dubois@epita.fr>
2017-10-24 17:24:19 +0200

    Initialize matrix and uses dot product

--- cpu/src/main.cc
+++ cpu/src/main.cc
@@ -2,8 +2,12 @@
 #include <armadillo>
 
 int main() {
-  arma::arma_rng::set_seed_random();
-  arma::Mat<double> A = arma::randu(4,4);
+
+  auto A = arma::Mat<float>(3, 3);
+  A = {{1, 2, 3},
+       {1, 2, 3},
+       {1, 2, 3}};
+
   std::cout << "A:\n" << A << "\n";
   std::cout << A * A.t() << "\n";
 }
