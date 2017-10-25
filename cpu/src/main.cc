63a3c15147206556b07d58c6dc5a40de544c27bf
thibaut <t.barroyer@me.com>
2017-10-25 15:43:43 +0200

    toto

--- cpu/src/main.cc
+++ cpu/src/main.cc
@@ -4,6 +4,28 @@
 # include "model.hh"
 
 int main() {
+
+  Model M;
+  M.add(3, 2);
+  M.add(2);
+
+  arma::Mat<float> X(4, 2);
+  X = {{0, 0},
+       {0, 1},
+       {1, 0},
+       {1, 1}};
+
+  arma::Mat<float> y(2, 4);
+  y = {{-1, -1, -1, -1},
+       {1, 1, 1, 1}};
+  y = y.t();
+
+  M.train(X, y, 1);
+
+  std::cout << M.forward(X) << std::endl;
+  std::cout << y << std::endl;
+  
+/*
   Model M;
   M.add(5, 2);
   M.add(1);
@@ -14,14 +36,15 @@ int main() {
        {1, 0},
        {1, 1}};
 
-  arma::Mat<float> y(1, 4);
-  y = {{0, 1, 1, 0}};
+  arma::Mat<float> y(2, 4);
+  y = {{1, -1, -1, 1}};
   y = y.t();
 
-  M.train(X, y, 3);
+  M.train(X, y, 10);
 
   std::cout << M.forward(X) << std::endl;
   std::cout << y << std::endl;
+*/
 
   return 0;
 }
--- cpu/src/model.cc
+++ cpu/src/model.cc
@@ -6,17 +6,19 @@
 void Model::init_W(size_t m, size_t n) {
   arma::Mat<float> M;
   M.randu(m, n);
-  float r = 4.0 * sqrt(6.0 / (m + n));
-  M *= r;
+  //float r = 4.0 * sqrt(6.0 / (m + n));
+  //M *= r;
   this->W.emplace_back(M);
 }
 
 arma::Mat<float> Model::sigmoid_mat_(arma::Mat<float>& matrix) {
-  return matrix.transform( [](double x) { return 1.0 / (1.0 + std::exp(-x)); } );
+  return matrix.transform( [](double x) { return (std::exp(2 * x) - 1) / (std::exp(2 * x) + 1); } );
 }
 
 arma::Mat<float> Model::dsigmoid_mat_(arma::Mat<float>& matrix) {
-  return matrix.transform( [](double x) { return x * (1.0 - x); } );
+  return matrix.transform( [](double x) {
+      float h = (std::exp(2 * x) - 1) / (std::exp(2 * x) + 1);
+      return 1 - h * h; } );
 }
 
 void Model::add(size_t output_units, size_t input_units) {
@@ -57,30 +59,42 @@ arma::Mat<float> Model::forward_keep(const arma::Mat<float>& X) {
   arma::Mat<float> tmp = arma::ones<arma::Mat<float>>(X_.n_rows, 1);
   X_.insert_cols(X_.n_cols, tmp);
 
-  this->C.push_back(X_);
-
   for (auto& W_ : this->W)
   {
     arma::Mat<float> X_(X_c);
     arma::Mat<float> tmp = arma::ones<arma::Mat<float>>(X_.n_rows, 1);
     X_.insert_cols(X_.n_cols, tmp);
+    this->C.push_back(arma::Mat<float>(X_));
 
     arma::Mat<float> o = X_ * W_;
+
     this->H.push_back(arma::Mat<float>(o));
 
     X_c = this->sigmoid_mat_(o);
-    this->C.push_back(X_c);
+
   }
+  this->C.push_back(X_c);
   return X_c;
 }
 
 std::vector<arma::Mat<float>> Model::get_err(const arma::Mat<float> truth) {
-  arma::Mat<float> err0 = (truth - C.back()) * dsigmoid_mat_(H.back());
+  arma::Mat<float> err0 = (truth - C.back()) % dsigmoid_mat_(H.back());
+
   auto err_vec = std::vector<arma::Mat<float>>();
   err_vec.push_back(err0);
-  for (int i = W.size() - 1; i >= 0; --i) {
-    arma::Mat<float> tmp = this->W[i] * err_vec.back();
-    arma::Mat<float> err = arma::sum(tmp, 1) * dsigmoid_mat_(H[i]);
+
+
+  for (int i = W.size() - 2; i >= 0; --i) {
+    /*std::cout << "W+1" << std::endl;
+    std::cout << this->W[i+1] << std::endl;
+    std::cout << "err" << std::endl;
+    std::cout << err_vec.back().t() << std::endl;*/
+    arma::Mat<float> tmp = this->W[i + 1] * err_vec.back().t();
+
+    arma::Mat<float> tt = arma::sum(tmp, 0);
+
+    arma::Mat<float> err = tt * dsigmoid_mat_(H[i]);
+
     err_vec.push_back(err);
   }
 
@@ -92,21 +106,29 @@ void Model::back_propagate(float lambda, const arma::Mat<float> truth) {
   auto err = get_err(truth);
 
   for (size_t i = 0; i < this->W.size(); ++i) {
-    std::cout << C[i] << std::endl;
-    std::cout << err[i] << std::endl;
-    this->W[i] += lambda * err[i].t() * this->C[i];
+    //std::cout << i << std::endl;
+    //std::cout << W[i] << std::endl;
+    arma::Mat<float> tmp = (lambda * err[i].t() * this->C[i]);
+    std::cout << tmp << std::endl;
+    this->W[i] += tmp.t();
+    //std::cout << W[i] << std::endl;
   }
 }
 
 void Model::train(arma::Mat<float>& X, arma::Mat<float>& y, size_t nb_epoch) {
-  for (;nb_epoch > 0; nb_epoch--)
+  for (size_t i = 0; i < nb_epoch; i++)
   {
+    std::cout << "============ EPOCH " << i << "\n";
     // TODO SUFFLE DATA
 
     for (size_t j = 0; j < X.n_rows; j++)
     {
+      //std::cout << j << "\n";
+      //std::cout << X.row(j) << "\n";
       this->forward_keep(X.row(j));
-      this->back_propagate(0.1, y.row(j));
+      //std::cout << y.row(j) << "\n";
+      this->back_propagate(1, y.row(j));
     }
+    //std::cout << this->forward(X) << "\n";
   }
 }
